d9115fa4baf700d0bcbaa56c0d5d90a861f1cab2
DimaTch <4dnik@mail.ru>
2022-05-01 11:52:13 +0300

    init project

---
+++ src/main/java/telran/charge/monitoring/ChargeMonitoringApplication.java
@@ -0,0 +1,13 @@
+package telran.charge.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargeMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/telran/charge/monitoring/ChargeMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.charge.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
