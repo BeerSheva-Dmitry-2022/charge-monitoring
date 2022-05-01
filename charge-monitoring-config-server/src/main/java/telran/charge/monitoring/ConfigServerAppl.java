20a9bfaab67d67b7e6e77271fda4d811413cbfdd
DimaTch <4dnik@mail.ru>
2022-05-01 12:16:28 +0300

    adds config server

--- charge-monitoring-config-server/src/main/java/telran/charge/monitoring/ConfigServerAppl.java
+++ charge-monitoring-config-server/src/main/java/telran/charge/monitoring/ConfigServerAppl.java
@@ -2,12 +2,14 @@ package telran.charge.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class ChargeMonitoringApplication {
+@EnableConfigServer
+public class ConfigServerAppl {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChargeMonitoringApplication.class, args);
+        SpringApplication.run(ConfigServerAppl.class, args);
     }
 
 }
---
+++ charge-monitoring-config-server/src/test/java/telran/charge/monitoring/ChargeMonitoringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.charge.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeMonitoringConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
